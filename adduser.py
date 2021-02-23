4f25a426ad5c64793574203e59dbc1a71d3cb729
root <root@video.umabot.ai>
2021-02-23 10:11:41 +0100

    utility to add an user with argon2 hashing for the password storage

---
+++ adduser.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+# Auto-upload Server - Utility to add an user to a password file
+from argon2 import PasswordHasher
+import sys
+import getopt
+import os
+
+#function to print the usage parameters
+def print_usage():
+    print("python3 adduser.py --username=<username> --password=<password> --filename=<passwordfilename>")
+    print("or")    
+    print("python3 adduser.py -u <username> -p <password> -f <passwordfilename>")
+    return
+# function to check the presence of username in the password file    
+def check_free_username(username,filename):
+    flag=True
+    if os.path.exists(filename)==False:
+       return(True)
+    f=open(filename, "rt")
+    while(True):
+        r=f.readline()
+        if not r:
+          break
+        v=r.split("#")
+        if v[0]==username:
+            flag=False
+    f.close()
+    return flag
+    
+# default settings
+username=""
+password=""
+filename=""
+try:
+   opts, args = getopt.getopt(sys.argv[1:],"hu:p:f",["username=","password=","filename="])
+except getopt.GetoptError:
+   print_usage()
+   sys.exit(2)
+for opt, arg in opts:
+   if opt in ("-h", "--help"):
+      print_usage()
+      sys.exit()
+   elif opt in ("-u", "--username"):
+      username = arg
+   elif opt in ("-p", "--password"):
+      password = arg
+   elif opt in ("-f", "--filename"):
+      filename = arg
+      
+if len(username)==0 or len(password)==0 or len(filename)==0:
+   print_usage()
+   sys.exit(2)
+if check_free_username(username,filename)==False:
+    print("[Error] Username is already used")
+    sys.exit(2)
+# encode with argon2 the password
+hash=PasswordHasher().hash(password)
+w=username+"#"+hash+"\n"
+print("[Info] Writing file: "+filename)
+f=open(filename, "a+")
+f.write(w)
+f.close()
+print("[Info] New user added");
+sys.exit(0)
+
+
+

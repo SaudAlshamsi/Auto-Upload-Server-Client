38413ef2b381ba579e04f5464981e7475d1ec3c2
root <root@video.umabot.ai>
2021-02-24 11:51:57 +0100

    removed an unused line

--- auto-upload-server.py
+++ auto-upload-server.py
@@ -186,7 +186,6 @@ if not os.path.exists(privatekey):
 #server starting
 print("[Info] Auto-upload-server starting")
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-#context.load_cert_chain('/etc/letsencrypt/live/upload.umabot.ai/fullchain.pem', '/etc/letsencrypt/live/upload.umabot.ai/privkey.pem')
 context.load_cert_chain(certificate, privatekey)
 
 #waiting for connections on port 443
